8d00475ab096165675ceba19f58af39c4b0e5fa4
TongyanXu <tyxu18@gmail.com>
2021-04-25 01:46:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyqttable",
-    version="0.0.1",
+    version="0.0.2",
     author="Tongyan Xu",
     author_email="tyxu18@gmail.com",
     description="A simple configurable table widget based on PyQt5 and pandas",
